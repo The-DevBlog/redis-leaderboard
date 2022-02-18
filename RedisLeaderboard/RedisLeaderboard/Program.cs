a49eab414863fb44d37decf7313861d878eafb8f
Andrew Smith <andrewcs149@gmail.com>
2022-02-17 17:55:39 -0800

    Add startup config.

--- RedisLeaderboard/RedisLeaderboard/Program.cs
+++ RedisLeaderboard/RedisLeaderboard/Program.cs
@@ -1,11 +1,12 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using RedisLeaderboard.Interfaces;
+using RedisLeaderboard.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ILeaderboardEntryService, LeaderboardEntryService>();
 
 // redis configuration
 builder.Services.AddStackExchangeRedisCache(options =>
