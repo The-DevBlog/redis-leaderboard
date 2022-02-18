b00fd10a162f8f4b4685a50f4c7743bd7a4084c6
Andrew Smith <andrewcs149@gmail.com>
2022-02-17 16:02:13 -0800

    Add Leaderboard class.

---
+++ RedisLeaderboard/RedisLeaderboard/Models/LeaderboardEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace RedisLeaderboard.Models
+{
+    public class LeaderboardEntry
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+        public LeaderboardEntry(string username, int score)
+        {
+            Username = username;
+            Score = score;
+        }
+    }
+}
