305bb63fe29c0a2500457f1d2351c1e3c9eb68d7
Andrew Smith <andrewcs149@gmail.com>
2022-02-17 17:40:37 -0800

    Add redis cache extensions class.

---
+++ RedisLeaderboard/RedisLeaderboard/RedisExtensions/DistributeCache.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace RedisLeaderboard.RedisExtensions
+{
+    // class used from Tim Corey's YouTube tutorial: https://www.youtube.com/watch?v=UrQWii_kfIE
+    public static class DistributeCache
+    {
+        public static async Task Set<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T> GetRecords<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+            var deserializedData = JsonSerializer.Deserialize<T>(jsonData);
+
+            return jsonData == null ? default(T) : deserializedData;
+        }
+    }
+}
