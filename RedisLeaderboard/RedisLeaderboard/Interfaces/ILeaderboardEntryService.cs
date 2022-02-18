de05c090b19d50d6b01788cd9587543f68ae763e
Andrew Smith <andrewcs149@gmail.com>
2022-02-17 16:15:08 -0800

    add interfaces folder.

---
+++ RedisLeaderboard/RedisLeaderboard/Interfaces/ILeaderboardEntryService.cs
@@ -0,0 +1,9 @@
+﻿using RedisLeaderboard.Models;
+
+namespace RedisLeaderboard.Interfaces
+{
+    public interface ILeaderboardEntryService
+    {
+        Task<LeaderboardEntryModel> GetEntries();
+    }
+}
