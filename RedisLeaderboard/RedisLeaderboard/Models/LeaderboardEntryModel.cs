18aa04c6b15ac4d1b30b95521e75fb3e7ae64c7b
Andrew Smith <andrewcs149@gmail.com>
2022-02-18 22:17:51 -0800

    Add annotation validator to input fields.

--- RedisLeaderboard/RedisLeaderboard/Models/LeaderboardEntryModel.cs
+++ RedisLeaderboard/RedisLeaderboard/Models/LeaderboardEntryModel.cs
@@ -1,9 +1,12 @@
-﻿namespace RedisLeaderboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedisLeaderboard.Models
 {
     public class LeaderboardEntryModel
     {
+        [Required]
         public string username { get; set; }
-        public int score { get; set; }
+        public int score { get; set; } = 0;
 
         public LeaderboardEntryModel(string username, int score)
         {
